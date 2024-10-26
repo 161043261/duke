d6929545c4c7d42ab8639f19758b73eb83a1c434
Tiancheng <yukino161043261@gmail.com>
2024-10-26 18:24:26 +0800

    Otaku

---
+++ ts-awesome/ts-proj1/src/components/hospital_top.vue
@@ -0,0 +1,79 @@
+<!-- Optional -->
+<script lang="ts">
+export default {
+  name: 'HospitalTop',
+}
+</script>
+
+<script setup lang="ts"></script>
+
+<template>
+  <!-- 顶部 -->
+  <div class="top">
+    <div class="content">
+      <!-- 左侧 -->
+      <div class="left">
+        <img src="../assets/logo.png" alt="logo" />
+        <p>Vue 医院 预约挂号统一平台</p>
+      </div>
+
+      <!-- 右侧 -->
+      <div class="right">
+        <p class="help">帮助中心</p>
+        <p class="login">登录/注册</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.top {
+  // 固定定位
+  position: fixed;
+  // 高层级
+  z-index: 777;
+  width: 100%;
+  height: 70px;
+  background-color: #fff;
+  // 弹性盒子
+  display: flex;
+  justify-content: center;
+
+  .content {
+    width: 1200px;
+    height: 70px;
+    background-color: #fff;
+    // 弹性盒子
+    display: flex;
+    justify-content: space-between;
+
+    .left {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      img {
+        width: 50px;
+        height: 50px;
+        margin-right: 10px;
+      }
+
+      p {
+        font-size: 20px;
+        color: #4490f1;
+      }
+    }
+
+    .right {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 14px;
+
+      .help {
+        margin-right: 10px;
+      }
+    }
+  }
+}
+</style>
---
+++ ts-awesome/ts-proj1/src/views/home/search.vue
@@ -0,0 +1,38 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+// 导入 element-plus 图标
+import { Search } from '@element-plus/icons-vue'
+</script>
+
+<template>
+  <div class="search">
+    <el-autocomplete clearable placeholder="请输入医院名" />
+    <el-button type="primary" size="default" :icon="Search">搜索</el-button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.search {
+  width: 100%;
+  height: 50px;
+  // background-color: red;
+  // 弹性盒子
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px 0;
+
+  // 深度选择器
+  // css  >>>
+  // less /deep/
+  // sass :deep()
+
+  // deprecated
+  // ::v-deep .el-autocomplete {
+  //   width: 600px;
+  // }
+  :deep(.el-autocomplete) {
+    width: 600px;
+  }
+}
+</style>
