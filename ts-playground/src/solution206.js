2fad1d10f127e1f45184890bd325be33f30a8f7e
yukino <yukino161043261@gmail.com>
2024-11-04 09:13:38 +0800

    Otaku

---
+++ ts-playground/src/solution206.js
@@ -0,0 +1,44 @@
+class ListNode {
+  val_
+  next_
+
+  /**
+   *
+   * @returns {number}
+   */
+  get val() {
+    return this.val_;
+  }
+
+  /**
+   *
+   * @param {number} newVal
+   */
+  set val(newVal) {
+    this.val_ = newVal;
+  }
+
+  /**
+   *
+   * @returns {ListNode}
+   */
+  get next() {
+    return this.next_;
+  }
+
+  /**
+   *
+   * @param {ListNode} newNext
+   */
+  set next(newNext) {
+    this.next_ = newNext;
+  }
+}
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+
+};
