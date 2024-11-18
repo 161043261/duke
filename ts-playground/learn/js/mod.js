25fa7f02d4a5f37701ed576448e2e543c7a98fe9
yukino <yukino161043261@gmail.com>
2024-11-18 14:26:40 +0800

    Otaku

--- ts-playground/learn/js/mod.js
+++ ts-playground/learn/js/mod.js
@@ -3,18 +3,17 @@
  * @param {number} y
  * @returns {number}
  */
-function add(x, y) {
-  return x + y
-}
+function add(x, y) { return x + y }
 
 console.log(module.exports === exports)
 
+// module.exports 指向新的对象, exports 仍指向原对象
 module.exports = {
   add,
   username: 'wft_name'
 }
 
-//! module.exports !== exports
+console.log(module.exports === exports)
 
 // comment next line
 exports = module.exports
@@ -22,3 +21,5 @@ exports = module.exports
 exports.password = 'wtf_pwd'
 
 console.log(module.exports === exports)
+
+// const mod = require('./mod')
