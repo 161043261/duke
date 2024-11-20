05ec116734c31dd90f9b7157bd3f7e173cb1112f
yukino <yukino161043261@gmail.com>
2024-11-21 01:05:05 +0800

    Otaku

--- cc-playground/src/solution.cpp
+++ cc-playground/src/solution.cpp
@@ -1,50 +1,82 @@
 #include <algorithm>
 #include <iostream>
+#include <mutex>
 #include <vector>
 
 using namespace std;
 
 int main() {
-  const string A = "xzyzzyx";
-  const string B = "zxyyzxz";
+    const string A = "xzyzzyx";
+    const string B = "zxyyzxz";
 
-  vector dp(A.length() + 1, vector(B.length() + 1, 0));
+    vector dp(A.length() + 1, vector(B.length() + 1, 0));
 
-  for (int i = 1; i <= A.length(); i++) {
-    for (int j = 1; j <= B.length(); j++) {
-      if (A[i - 1] == B[j - 1]) {
-        dp[i][j] = dp[i - 1][j - 1] + 1;
-      } else {
-        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-      }
+    for (int i = 1; i <= A.length(); i++) {
+        for (int j = 1; j <= B.length(); j++) {
+            if (A[i - 1] == B[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
     }
-  }
 
-  int cur = 0;
-  string ans;
-  const unsigned long range = min(A.length(), B.length());
+    int cur = 0;
+    string ans;
+    const unsigned long range = min(A.length(), B.length());
 
-  for (int i = 1, j = 1; i <= range; i++) {
-    int j_ = j;
-    for (; j <= i; j++) {
-      if (dp[i][j] == cur + 1) {  // 找到
-        // cout << i - 1 << " " << j - 1 << endl;
-        cur++;
-        ans += A[i - 1];
-        break;
-      }
+    for (int i = 1, j = 1; i <= range; i++) {
+        int j_ = j;
+        for (; j <= i; j++) {
+            if (dp[i][j] == cur + 1) { // 找到
+                // cout << i - 1 << " " << j - 1 << endl;
+                cur++;
+                ans += A[i - 1];
+                break;
+            }
+        }
+        j = j_; // 未找到, 复位
     }
-    j = j_;  // 未找到, 复位
-  }
 
-  cout << "dp: " << endl;
-  for (const auto& row : dp) {
-    for (const int val : row) {
-      cout << val << " ";
+    cout << "dp: " << endl;
+    for (const auto &row : dp) {
+        for (const int val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
-  }
 
-  cout << "ans: " << ans << endl;
-  return 0;
+    cout << "ans: " << ans << endl;
+    return 0;
 }
+
+// FIXME
+class Foo {
+  public:
+    mutex mut1, mut2, mut3;
+    Foo() /* : mut1(mutex{}), mut2(mutex{}), mut3(mutex{}) */ {
+        mut2.lock();
+        mut3.lock();
+    }
+
+    void first(function<void()> printFirst) {
+        mut1.lock();
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        mut2.unlock();
+    }
+
+    void second(function<void()> printSecond) {
+        mut2.lock();
+        // printSecond() outputs "second". Do not change or remove this line.
+        printSecond();
+        mut3.unlock();
+    }
+
+    void third(function<void()> printThird) {
+        mut3.lock();
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
+        mut1.unlock();
+    }
+};
--- ts-playground/learn/js/closure.js
+++ ts-playground/learn/js/closure.js
@@ -11,20 +11,47 @@ counter()
 counter()
 counter()
 
-// 闭包 = 内层函数 + 外层函数的局部变量
-// 优点: 实现数据私有
-// 缺点: 可能有内存泄露
+// 闭包 = 内层函数 + 外层函数的变量
+// 优点: 封闭数据
+// 缺点: 可能会内存泄露
 
+//! var 变量提升: 允许变量在声明前访问
+// let, const 没有变量提升
 console.log(str) // undefined
 var str = 'Hello'
 
-// 函数提升
-foo() // 函数可以在声明前调用
+//! 具名函数提升: 允许函数在声明前调用
+// 函数表达式 (匿名函数), lambda 表达式 (箭头函数) 没有函数提升
+foo()
+
 function foo() {
   console.log('foo')
 }
 
-bar() // 函数表达式不可以在声明前调用
-const bar = function() {
-  console.log('bar')
+try {
+  bar1()
+  const bar1 = function () {
+    console.log('bar1')
+  }
+} catch (e) {
+  console.log(e.toString().slice(16))
 }
+
+try {
+  bar2()
+  const bar2 = () => {
+    console.log('bar2')
+  }
+} catch (e) {
+  console.log(e.toString().slice(16))
+}
+
+//! 参数默认值
+function print(name = 'foo', age) {
+  console.log(`name: ${name}, age: ${age}`)
+}
+
+print() // name: foo, age: undefined
+print('bar') // name: bar, age: undefined
+print('bar', 3.5) // name: bar, age: 3.5
+
