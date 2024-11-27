53cb18fbc8f57637646b35265830f569e8c3eaec
Tiancheng <yukino161043261@gmail.com>
2024-11-27 22:34:53 +0800

    Otaku

--- ts-playground/src/draft/solution.js
+++ ts-playground/src/draft/solution.js
@@ -1,1 +1,11 @@
-
+try {
+  throw new AggregateError(
+    [new Error("www"), new Error("ttt"), new Error("fff")],
+    "wtf",
+  );
+} catch (e) {
+  console.log(e instanceof AggregateError); // true
+  console.log(e.message); // wtf
+  console.log(e.name); // AggregateError
+  console.log(e.errors); // [ Error: www, Error: ttt, Error: fff ]
+}
