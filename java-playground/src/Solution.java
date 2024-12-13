9b6b2a58c5d0fc79de4486003c8d91e7e8ed030c
Tiancheng <yukino161043261@gmail.com>
2024-12-13 14:04:25 +0800

    Otaku

--- java-playground/src/Solution.java
+++ java-playground/src/Solution.java
@@ -1,54 +1,22 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
 class Solution {
 
-  public int countGoodNodes(int[][] edges) {
-    int n = edges.length + 1;
-    // g: idx parent/child
-    ArrayList<Integer>[] g = new ArrayList[n];
-
-    // Arrays.fill() 后, g 的元素是相同的 ArrayList
-    // Arrays.fill(g, new ArrayList<Integer>());
-
-    Arrays.setAll(g, idx -> new ArrayList<>());
-    for (var edge : edges) {
-      int x = edge[0];
-      int y = edge[1];
-      g[x].add(y);
-      g[y].add(x);
-    }
-
-    // System.out.println(Arrays.deepToString(g));
-
-    dfs(0, -1, g);
-    return ans;
-  }
-
-  private int ans;
-
-  private int dfs(int x/* son */, int fa/* father */, ArrayList<Integer>[] g) {
-    int size = 1; //
-    int sz0 = 0;
-    boolean ok = true;
-    for (int y : g[x]) {
-      if (y == fa) {
-        continue;
+  public int[] getFinalState(int[] nums, int k, int multiplier) {
+    for (var i = 0; i < k; i++) {
+      var minNum = nums[0];
+      var minIdx = 0;
+      for (var idx = 1; idx < nums.length; idx++) {
+        if (nums[idx] < minNum) {
+          minNum = nums[idx];
+          minIdx = idx;
+        }
       }
-      int sz = dfs(y, x, g);
-      if (sz0 == 0) {
-        sz0 = sz;
-      } else if (sz != sz0) {
-        ok = false;
-      }
-      size += sz;
+      nums[minIdx] *= multiplier;
+      // System.out.println(Arrays.toString(nums));
     }
-    ans += ok ? 1 : 0;
-    return size;
+    return nums;
   }
 
-  public static void main(String[] args) {
-    var edges = new int[][]{{0, 1}, {0, 2}, {1, 3}, {1, 4}, {2, 5}, {2, 6}};
-    System.out.println(new Solution().countGoodNodes(edges));
-  }
+  // public static void main(String[] args) {
+  //   new Solution().getFinalState(new int[] { 2, 1, 3, 5, 6 }, 5, 2);
+  // }
 }
