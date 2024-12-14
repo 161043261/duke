1ee27ee0ad9b37239d8a7c4a69bc5cc0addcf6c1
Tiancheng <yukino161043261@gmail.com>
2024-12-14 14:11:52 +0800

    Otaku

--- ts-playground/src/exercise/2650.js
+++ ts-playground/src/exercise/2650.js
@@ -1,40 +1,86 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /**
  * @param {Generator} generator
  * @return {[Function, Promise]}
  */
-let cancellable = function (generator) {
+const cancellable = function (generator) {
   let cancel;
   const p = new Promise(function (resolve, reject) {
     cancel = function () {
-      run("Cancelled", "throw")
-    }
+      run("Cancelled", "throw");
+    };
     const run = function (arg, fn = "next") {
       try {
         let ret = generator[fn](arg);
         if (ret.done) {
-          return resolve(ret.value)
+          return resolve(ret.value);
         }
-        ret.value.then((value) => {
-          run(value)
-        }).catch((reason) => {
-          run(reason, 'throw')
-        })
+        ret.value
+          .then((value) => {
+            run(value);
+          })
+          .catch((reason) => {
+            run(reason, "throw");
+          });
       } catch (e) {
-        reject(e)
+        reject(e);
       }
-    }
+    };
     run(null);
-  })
-  return [cancel, p]
+  });
+  return [cancel, p];
 };
 
 function* tasks() {
-  const val = yield new Promise(resolve => resolve(2 + 2));
-  yield new Promise(resolve => setTimeout(resolve, 100));
-  return val + 1;
+  try {
+    yield new Promise((resolve, reject) => reject("Promise Rejected"));
+  } catch (e) {
+    let a = yield new Promise(resolve => resolve(2));
+    let b = yield new Promise(resolve => resolve(2));
+    return a + b;
+  }
 }
 
-const [cancel, promise] = cancellable(tasks());
-setTimeout(cancel, 50);
-promise.catch(console.log); // logs "Cancelled" at t=50ms
+const [cancel, promise] = cancellable1(tasks());
+setTimeout(cancel, null);
+promise.catch(console.log);
 
+function cancellable1(generator) {
+  let cancel;
+  const p = new Promise(function (resolve, reject) {
+    cancel = function () {
+      try {
+        let ret = generator.throw("Cancelled");
+        if (ret.done) {
+          return resolve(ret.value);
+        }
+      } catch (e) {
+        reject(e);
+      }
+    };
+    const run = function (arg, doNext = true) {
+      try {
+        let ret
+        if (doNext) {
+          ret = generator.next(arg);
+        } else {
+          ret = generator.throw(arg);
+        }
+        if (ret.done) {
+          return resolve(ret.value);
+        }
+        ret.value
+          .then((value) => {
+            run(value);
+          })
+          .catch((reason) => {
+            run(reason, false);
+          });
+      } catch (e) {
+        reject(e);
+      }
+    };
+    run(null);
+  });
+  return [cancel, p];
+}
--- ts-playground/src/exercise/2722.js
+++ ts-playground/src/exercise/2722.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /**
  * @param {Array} arr1
  * @param {Array} arr2
